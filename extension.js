873678bb9e9ca066ed2d233b2a350acef5a0bea3
FranX1024 <52638847+FranX1024@users.noreply.github.com>
2020-12-14 12:57:22 +0100

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Always on focus
+// @namespace     http://www.webmonkey.com
+// @description   Makes sites unable to figure out whether you've left focus on them
+// @match         *://*/*
+// @author        anonimni hakerman
+// ==/UserScript==
+
+console.log('Always on focus extension is working.');
+Object.defineProperty(document, "hidden", { value : false});
